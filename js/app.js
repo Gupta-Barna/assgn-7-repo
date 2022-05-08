2166ab063ccdca106197b1c1d83a50aca4eae25a
Gupta-Barna <dasguptabarna125@gmail.com>
2022-05-08 23:35:20 +0600

    bug added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+ 
 let posts=[ ];
 
 const likedPostsId = [];
@@ -16,7 +17,7 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-    likedPostsId.plus(id); 
+    likedPostsId.push(id); 
     showPosts(posts);
 };
 
@@ -27,7 +28,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-    return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+    return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
@@ -62,16 +63,14 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
-
                 <button class="post__more-options">
                   <i class="fa-solid fa-ellipsis"></i>
                 </button>
               </div>
-
               <div class="post__content">
                 <div class="post__medias">
                   <img
@@ -81,7 +80,6 @@ const createPost = (post) => {
                   />
                 </div>
               </div>
-
               <div class="post__footer">
                 <div class="post__buttons">
                   <button class="post__button" onclick="addToLiked(${post.id})">
@@ -92,37 +90,30 @@ const createPost = (post) => {
                     <i class="fa-solid fa-comment"></i>
                   </button>
                   
-
                   <div class="post__indicators"></div>
-
                   <button class="post__button post__button--align-right" onclick="reportPost(${
                       post.id
                   })">
                     <i class="fa-solid fa-ban"></i>
                   </button>
                 </div>
-
                 <div class="post__content">${displayContent(post.description)}</div>
-
                 <div class="post__infos">
                   <div class="post__likes">
                     <a href="#" class="post__likes-avatar">
                       <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="User Picture" />
                     </a>
-
                     <span>Liked by
                       <a class="post__name--underline" href="#">user123</a> and
                       <a href="#">73 others</a></span>
                   </div>
-
                   <hr/>
-
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
@@ -144,15 +135,18 @@ const showPosts = (posts) => {
 
 const displayLikedPosts = () => {
     const likedPosts = getLikedPosts();
+    document.getElementById( "liked" ).textContent = "";
     likedPosts.forEach((post) => {
         const div = createPost(post);
-        document.getElementById( "liked" ).appendChild(div);
+              document.getElementById( "liked" ).appendChild(div);
+
     });
 };
 
 const displayReportedPosts = () => {
     const reportedPosts = getReportedPosts();
-    posts.forEach((post) => {
+    document.getElementById( "reported" ).textContent = "";
+    reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
     });
@@ -164,4 +158,4 @@ const loadPosts = async () =>{
   showPosts(posts);
 }
 
-loadPosts();
+
